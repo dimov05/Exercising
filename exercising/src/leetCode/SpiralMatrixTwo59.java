47678e42afe716ef0141629887e747378f8791fe
dimov05 <ddimov.1999@gmail.com>
2023-05-11 10:20:19 +0300

    Remove duplicates from sorted array Leet code problem #26

---
+++ exercising/src/leetCode/SpiralMatrixTwo59.java
@@ -0,0 +1,41 @@
+package leetCode;
+
+// Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+public class SpiralMatrixTwo59 {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int number = 1;
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = number;
+                number++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = number;
+                number++;
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = number;
+                    number++;
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = number;
+                    number++;
+                }
+                left++;
+            }
+        }
+        return matrix;
+    }
+}
