a39f7783df07809b433b478197b64f1211444e77
dimov05 <ddimov.1999@gmail.com>
2023-05-11 10:19:47 +0300

    Majority element Leet code problem #169

--- exercising/src/leetCode/MajorityElement169.java
+++ exercising/src/leetCode/MajorityElement169.java
@@ -5,7 +5,7 @@ package leetCode;
 //        The majority element is the element that appears more than ⌊n / 2⌋ times.
 //        You may assume that the majority element always exists in the array.
 
-public class majorityElement169 {
+public class MajorityElement169 {
     public int majorityElement(int[] nums) {
         int count = 0;
         int majorNumber = nums[0];
