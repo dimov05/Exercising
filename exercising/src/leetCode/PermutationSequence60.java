5977513202f3588c347289792f872bdbb8dc304a
dimov05 <ddimov.1999@gmail.com>
2023-05-15 10:29:52 +0300

    Permutation Sequence - Leet code problem #60

---
+++ exercising/src/leetCode/PermutationSequence60.java
@@ -0,0 +1,26 @@
+package leetCode;
+
+import java.util.ArrayList;
+
+public class PermutationSequence60 {
+    public String getPermutation(int n, int k) {
+        ArrayList<Integer> array = new ArrayList<>();
+        int fact = 1;
+        for(int i  = 1;i<n; i++){
+            array.add(i);
+            fact *= i;
+        }
+        k = k - 1;
+        StringBuilder s = new StringBuilder();
+        array.add(n);
+        while(true){
+            s.append(array.get(k/fact).toString());
+            if(array.size() == 0){
+                break;
+            }
+            k = k % fact;
+            fact = fact / array.size();
+        }
+        return s.toString();
+    }
+}
