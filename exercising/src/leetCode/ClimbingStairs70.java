31dd7a029e253c8226d79116f22aefa1971544a9
dimov05 <ddimov.1999@gmail.com>
2023-05-18 09:21:13 +0300

    Climbing Stairs - Leet Code problem #70

---
+++ exercising/src/leetCode/ClimbingStairs70.java
@@ -0,0 +1,21 @@
+package leetCode;
+//You are climbing a staircase. It takes n steps to reach the top.
+//
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+public class ClimbingStairs70 {
+    public int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        } else if(n == 2){
+            return 2;
+        } else {
+            int[] array = new int[n];
+            array[0] = 1;
+            array[1] = 2;
+            for(int i = 2;i<n;i++){
+                array[i] = array[i-1] + array[i-2];
+            }
+            return array[n-1];
+        }
+    }
+}
