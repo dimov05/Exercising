1c9f549e5e021fb5d98fc37ff066905e6901c074
dimov05 <ddimov.1999@gmail.com>
2023-05-18 09:21:34 +0300

    Index of First Occurrence in String - Leet Code problem #28

---
+++ exercising/src/leetCode/IndexOfFirstOccurrenceInString28.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+//Given two strings needle and haystack,
+// return the index of the first occurrence of needle in haystack,
+// or -1 if needle is not part of haystack.
+
+public class IndexOfFirstOccurrenceInString28 {
+    public int strStr(String haystack, String needle) {
+        int lengthNeedle = needle.length();
+        for (int i = 0; i <= haystack.length() - lengthNeedle; i++) {
+            String toCheck = haystack.substring(i, i + lengthNeedle);
+            if (toCheck.equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
