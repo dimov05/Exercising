6f16bbd41a53f9f09848908b157c7f51a0b5d021
dimov05 <ddimov.1999@gmail.com>
2023-05-19 10:28:20 +0300

    Binary Tree In Order Traversal - Leet Code problem #94

---
+++ exercising/src/leetCode/BinaryTreeInOrderTraversal94.java
@@ -0,0 +1,43 @@
+package leetCode;
+
+//Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInOrderTraversal94 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<Integer> res = new ArrayList<>();
+
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        res.add(root.val);
+        inorder(root.right);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return res;
+    }
+}
