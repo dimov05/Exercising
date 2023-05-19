6ccc5bf7e50c3c1d3d1e7548cdb55768868f6a6f
dimov05 <ddimov.1999@gmail.com>
2023-05-19 10:28:03 +0300

    Rotate List - Leet Code problem #61

--- exercising/src/leetCode/RotateList61.java
+++ exercising/src/leetCode/RotateList61.java
@@ -3,19 +3,19 @@ package leetCode;
 // Given the head of a linked list, rotate the list to the right by k places.
 class ListNode {
       int val;
-      ListNode next;
+      ListNode21 next;
       ListNode() {}
       ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      ListNode(int val, ListNode21 next) { this.val = val; this.next = next; }
   }
 
 public class RotateList61 {
-    public ListNode rotateRight(ListNode head, int k) {
+    public ListNode21 rotateRight(ListNode21 head, int k) {
         if(k == 0 || head == null || head.next == null){
             return head;
         }
         int lenght = 1;
-        ListNode current = head;
+        ListNode21 current = head;
         while(current.next != null){
             lenght++;
             current = current.next;
