185a6cea541fedaf05d4b1fdc4f8e6f834f84683
dimov05 <ddimov.1999@gmail.com>
2023-05-30 11:34:55 +0300

    Maximum Sum With Exactly K Elements - Leet Code Problem #2656

---
+++ exercising/src/leetCode/MaximumSumWithExactlyKELements2656.java
@@ -0,0 +1,29 @@
+package leetCode;
+
+//Example 1:
+//
+//Input: nums = [1,2,3,4,5], k = 3
+//Output: 18
+//Explanation: We need to choose exactly 3 elements from nums to maximize the sum.
+//For the first iteration, we choose 5. Then sum is 5 and nums = [1,2,3,4,6]
+//For the second iteration, we choose 6. Then sum is 5 + 6 and nums = [1,2,3,4,7]
+//For the third iteration, we choose 7. Then sum is 5 + 6 + 7 = 18 and nums = [1,2,3,4,8]
+//So, we will return 18.
+//It can be proven, that 18 is the maximum answer that we can achieve.
+
+public class MaximumSumWithExactlyKELements2656 {
+    public int maximizeSum(int[] nums, int k) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            sum += max;
+            max++;
+        }
+        return sum;
+    }
+}
