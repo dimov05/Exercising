3dc63c61270aeadbe55bb63c888c88eea81461a0
Dimo Dimov <ddimov.1999@gmail.com>
2023-07-21 14:54:07 +0300

    Implementing custom list

---
+++ exercising/src/academy/customList/CustomList.java
@@ -0,0 +1,153 @@
+package academy.customList;
+
+import java.util.Arrays;
+
+public class CustomList<T> {
+    private int size;
+    private int CAPACITY = 10;
+    private T[] elements;
+
+    public CustomList() {
+        this.size = 0;
+        this.elements = (T[]) new Object[CAPACITY];
+    }
+
+    public CustomList(int capacity) {
+        this.size = 0;
+        this.elements = (T[]) new Object[capacity];
+        this.CAPACITY = capacity;
+    }
+
+    public void add(T element) {
+        if (size == CAPACITY) {
+            this.CAPACITY = CAPACITY * 3 / 2 + 1;
+            T[] copyElements = (T[]) new Object[CAPACITY];
+            if (size >= 0) System.arraycopy(this.elements, 0, copyElements, 0, size);
+            this.elements = copyElements;
+
+            //TODO: make larger array and copy elements into it
+        }
+        if (element == null) {
+            throw new NullPointerException("Can't add element null");
+        }
+        this.elements[size] = element;
+        size++;
+    }
+
+    public void remove(int indexToRemove) {
+        if (indexToRemove < 0 || indexToRemove >= this.size) {
+            throw new IndexOutOfBoundsException("The given index is out of bounds! - " + indexToRemove);
+        }
+        for (int i = indexToRemove; i < size - 1; i++) {
+            this.elements[i] = this.elements[i + 1];
+        }
+        elements[size - 1] = null;
+        size--;
+    }
+
+    public void remove(T element) {
+        if (element == null) {
+            throw new NullPointerException("Can't remove element null");
+        }
+        int indexOfElement = indexOf(element);
+        if (indexOfElement == -1) {
+            throw new IllegalArgumentException("The element is not present in the List");
+        }
+        for (int i = 0; i < size; i++) {
+            if (this.elements[i] == element) {
+                remove(i);
+            }
+        }
+    }
+
+    public int indexOf(T element) {
+        if (element == null) {
+            throw new NullPointerException("Can't find index of element null");
+        }
+        int indexOfElement = -1;
+        for (int i = 0; i < size; i++) {
+            if (this.elements[i] == element) {
+                indexOfElement = i;
+                break;
+            }
+        }
+        return indexOfElement;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            this.elements[i] = null;
+        }
+        this.size = 0;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bound for size " + this.size);
+        }
+        return elements[index];
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public int lastIndexOf(T element) {
+        if (element == null) {
+            throw new NullPointerException("Can't find index of element null");
+        }
+        int indexOfElement = -1;
+        for (int i = size; i >= 0; i--) {
+            if (this.elements[i] == element) {
+                indexOfElement = i;
+                break;
+            }
+        }
+        if (indexOfElement == -1) {
+            throw new IllegalArgumentException("The element is not present in the List");
+        }
+        return indexOfElement;
+    }
+
+    public void addAll(CustomList<T> elementsToAdd) {
+        for (int i = 0; i < elementsToAdd.size; i++) {
+            if (elementsToAdd.get(i) == null) {
+                throw new NullPointerException("Can't add element null");
+            }
+            add(elementsToAdd.get(i));
+        }
+    }
+
+    public boolean contains(T element) {
+        if (element == null) {
+            throw new NullPointerException("Can't find element null");
+        }
+        return Arrays.stream(this.elements).filter(e -> e == element).toArray().length >= 1;
+    }
+
+    public boolean containsAll(CustomList<T> elementsToCheck) {
+        for (int i = 0; i < elementsToCheck.size; i++) {
+            if (elementsToCheck.get(i) == null) {
+                throw new NullPointerException("Can't check 'containsAll' for null element");
+            }
+            if (!contains(elementsToCheck.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.size; i++) {
+            sb.append(elements[i])
+                    .append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        return sb.toString();
+    }
+}
---
+++ exercising/src/academy/customList/Main.java
@@ -0,0 +1,41 @@
+package academy.customList;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomList<String> cList = new CustomList<>();
+        cList.add("Test 1");
+        cList.add("Test 2");
+        cList.add("Test 3");
+        System.out.println(cList + "  -- Added 3 elements");
+        cList.remove(1);
+        System.out.println(cList + "  -- Removed element at index 1");
+        cList.add("Test 4");
+        cList.add("Test 1");
+        System.out.println(cList + "  -- Added 2 more elements");
+        cList.remove("Test 1");
+        System.out.println(cList + "  -- Removed all elements by given element");
+        System.out.println(cList.get(1) + "  -- Get element at index 1");
+        CustomList<String> cListToAdd = new CustomList<>();
+        cList.add("Test 5");
+        cList.add("Test 4");
+        cList.add("Test 7");
+        cList.addAll(cListToAdd);
+        System.out.println(cList + "  -- Added 3 elements by add all");
+        System.out.println(cList.get(3) + "  -- get Element by index 3(Test 4)");
+        System.out.println(cList.getSize() + "  -- return size of list");
+        System.out.println(cList.indexOf("Test 5") + "  -- return index of element(Test 5)");
+
+        System.out.println(cList.lastIndexOf("Test 4") + "  -- returns last index of Element");
+        System.out.println(cList.contains("Test 4") + " -- Checks if list contains element (true)");
+        System.out.println(cList.contains("Test 23") + " -- Checks if list contains element (false)");
+        System.out.println(cList.containsAll(cListToAdd) + "  -- checks if list containsAll elements from other list (true)");
+        cListToAdd.add("Test 231231");
+        System.out.println(cList.containsAll(cListToAdd) + "  -- checks if list containsAll elements from other list (false)");
+        System.out.println(cList.getSize() + "  -- prints size");
+        cList.clear();
+        System.out.println(cList.getSize() + "  -- prints size after .clear");
+        System.out.println(cList.isEmpty() + "  -- checks if isEmpty (true)");
+        cList.addAll(cListToAdd);
+        System.out.println(cList.isEmpty() + "  -- checks if isEmpty (false)");
+    }
+}
