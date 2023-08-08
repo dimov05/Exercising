3a0af7f5f1c74bb6466ea164f134ef2b8236c2b6
Dimo Dimov <ddimov.1999@gmail.com>
2023-08-08 09:34:43 +0300

    Initialize Spring Dev Project

---
+++ spring-dev/src/test/java/com/springboot/demo/springdev/SpringDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.springdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
