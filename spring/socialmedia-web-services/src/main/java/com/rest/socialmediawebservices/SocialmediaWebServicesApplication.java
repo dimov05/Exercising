14df91a1ce23c6099c7720e1e60d97aec4419b0b
Dimo Dimov <ddimov.1999@gmail.com>
2023-08-17 10:35:34 +0300

    Initialize Spring project and dependencies

---
+++ spring/socialmedia-web-services/src/main/java/com/rest/socialmediawebservices/SocialmediaWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rest.socialmediawebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaWebServicesApplication.class, args);
+	}
+
+}
